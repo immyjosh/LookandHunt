e8eaa665f3758fa621862e0cd65c9f8760d74e0d
immyjosh <immanueljoshuapaul@gmail.com>
2018-10-26 16:17:30 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to LookandHunt</title>
+
+	<meta name="description" content="Search the web for sites and images.">
+	<meta name="keywords" content="Search engine, LookandHunt, websites">
+	<meta name="author" content="immanuel Joshua Paul">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<div class="wrapper indexPage">
+	
+
+		<div class="mainSection">
+
+			<div class="logoContainer">
+				<img src="assets/images/lookandhunt.png" title="Logo of our site" alt="Site logo">
+			</div>
+
+
+			<div class="searchContainer">
+
+				<form action="search.php" method="GET">
+
+					<input class="searchBox" type="text" name="term" autocomplete="off">
+					<input class="searchButton" type="submit" value="Search">
+
+
+				</form>
+
+			</div>
+
+
+		</div>
+
+
+	</div>
+
+</body>
+</html>
